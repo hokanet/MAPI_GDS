8bad4bbddcd7f55ddc0e0a5542ac957819fe0533
hokanet <76160688+hokanet@users.noreply.github.com>
2020-12-20 01:32:02 +0800

    enhance date

--- index.jsx
+++ index.jsx
@@ -55,11 +55,11 @@ class LatestCommitComponent extends React.Component {
     ) : (
       <div>
         <p>last committed:</p>
-        <p>by: </p><div id="author">{this.state.author}</div>
-        <div id="branch">{this.state.branch}</div>
-        <p>on: </p><div id="date">{this.state.date}</div>
-        <div id="sha">{this.state.sha}</div>
-        <div id="link">{this.state.link}</div>
+        //<div id="author">{this.state.author}</div>
+        //<div id="branch">{this.state.branch}</div>
+        <div id="date"><p>on: </p>{this.state.date}</div>
+        //<div id="sha">{this.state.sha}</div>
+        //<div id="link">{this.state.link}</div>
       </div>
     );
 
